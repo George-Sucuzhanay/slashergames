1afe214de1d58d2b816c545353760d5f496f39ad
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-08-11 11:10:45 -0400

    Modifiying sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,26 @@
 let img;
 let xPos = 290;
 let yPos = 410;
+let homePos;
+let officePos;
+let storePos;
+let bankPos;
 
 function preload(){
     img = loadImage("images/background.jpg");
 }
 
 function setup(){
-    createCanvas(700,700);
+    createCanvas(800,800);
     //Set background for Game
-    image(img, 0, 0, 700, 700);
+    // image(img, 0, 0, 700, 700);
 }
 
 function draw(){
+    image(img, 0, 0, 700, 700);
     //Player char is a red dot
+    fill(255, 0, 0);
+    noStroke();
     circle(xPos, yPos, 25);
     //Update position using keyboard buttons
     if(keyIsDown(87)){
