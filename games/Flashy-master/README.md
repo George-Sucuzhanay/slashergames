59ab3781676fb522f4abfc59824e1b2792f0a6cc
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-08-12 10:39:08 -0400

    Exciting features and new games

---
+++ games/Flashy-master/README.md
@@ -0,0 +1,19 @@
+# Flashy 🔦
+
+Flashy is a simple 2D game. You have to pick up items to survive and avoid contact with enemies.
+But be careful. You are just equipped with a flashlight to light up the maze.
+
+![flashy-screenshot](https://raw.githubusercontent.com/Milchreis/Flashy/master/flashy.gif)
+
+Play here: https://milchreis.github.io/Flashy/
+
+## Rules
+
+ - Find the turquoise treasure to stay alive and gain points
+ - Don't touch the violet agents
+ - On each pick up a treasure, 2 more agents spawn and all agents are getting a little faster
+
+## Background
+
+Flashy emerged from a coding challenge and was developed into a game. With the game I wanted to gain some experience with 2D-Ray-Casting and Steering-Behaviors. 
+Special thanks goes to Danial Shiffman for the great coding videos, the code base and the inspiration for this project.
--- sketch.js
+++ sketch.js
@@ -1,10 +1,21 @@
 let img;
-let xPos = 290;
-let yPos = 410;
-let homePos;
-let officePos;
-let storePos;
-let bankPos;
+let xPos = 178;
+let yPos = 95;
+let homeXPos = 178;
+let homeYPos = 91;
+let officeXPos = 1589;
+let officeYPos = 110;
+let storeXPos = 1557;
+let storeYPos = 410;
+let bankXPos = 142;
+let bankYPos = 403;
+let pizzaXPos = 986;
+let pizzaYPos = 388;
+let speed = 5;
+let state = 2;
+let money = 100;
+let storeEvent = 1;
+let 
 
 function preload(){
     img = loadImage("images/background.jpg");
@@ -12,29 +23,106 @@ function preload(){
 
 function setup(){
     createCanvas(1700,700);
+    //Assign position values to each of the locations
+    // let playButton = document.createElement("h1");
+    // playButton.innerHTML('jsdjvjdvnb');
+    // playButton.appendChild(canvas)
     //Set background for Game
     // image(img, 0, 0, 700, 700);
 }
 
 function draw(){
-    image(img, 0, 0, 1700, 700);
-    //Player char is a red dot
-    fill(255, 0, 0);
-    noStroke();
-    circle(xPos, yPos, 25);
-    //Update position using keyboard buttons
-    if(keyIsDown(87)){
-        yPos--;
-    }
-    if(keyIsDown(83)){
-        yPos++;
-    }
-    if(keyIsDown(65)){
-        xPos--;
-    }
-    if(keyIsDown(68)){
-        xPos++;
-    }
-    //Add random events in map, smthn  to be done after above
 
+    if(state === 1){
+       
+        let start = document.querySelector("h3");
+        let canvas = document.querySelector("canvas");
+        let startDiv = document.getElementById("play");
+        canvas.style.display = "none";
+        
+       start.onclick = function(){
+        console.log('20px');
+        state = 2;
+        startDiv.style.display = "none";
+        canvas.style.display = "block";
+       }
+       
+    } 
+    
+    else if (state === 2){
+      
+        let rng = random(10);
+        let r = random(255);
+        let g = random(255);
+        let b = random(255);
+ 
+        rectMode(RADIUS);
+        ellipseMode(CENTER);
+        image(img, 0, 0, 1700, 700);
+        //Player char is a red dot
+        fill(255, 0, 0);
+        noStroke();
+        circle(xPos, yPos, 25);
+        //Update position using keyboard buttons
+        if(keyIsDown(87)){
+        yPos--;
+        }
+        if(keyIsDown(83)){
+        yPos++;
+        }    
+        if(keyIsDown(65)){
+        xPos--;
+        }
+        if(keyIsDown(68)){
+        xPos++;
+        }
+        //Add random events in map, smthn  to be done after above
+        if(xPos < homeXPos - 30 || yPos < homeYPos - 30 || xPos > homeXPos + 40 || yPos > homeYPos + 30 ){
+            text("There's an attempt a break in attempt at your house!", 850, 700);
+            fill(r, g, b);
+            square(homeXPos, homeYPos, 50);
+            console.log("player is home")
+        }
+        else if(xPos < officeXPos - 30 || yPos < officeYPos - 30 || xPos > officeXPos + 40 || yPos > officeYPos + 30){
+            text("It's a hostage  situation at the office", 850, 700);
+            fill(r, g, b);
+            circle(officeXPos, officeYPos, 20);;   
+            console.log('player is at the office');         
+        }
+        else if((xPos < storeXPos - 30 || yPos < storeYPos - 30 || xPos > storeXPos + 40 || yPos > storeYPos + 30) && storeEvent == 1){
+            text("Stick em up! It's a robbery!", 850, 700);
+            fill(r, g, b);
+            ellipse(storeXPos, storeYPos, 40, 20);
+            console.log('player is at the stor');
+            
+        }
+        else if(xPos < bankXPos - 30 || yPos < bankYPos - 30 || xPos > bankXPos + 40 || yPos > bankYPos + 30){
+            text("Bonnie and Clyde are back at it again!", 850, 700);
+            fill(r, g, b);
+            square(bankXPos, bankYPos, 35);
+            console.log('player is at the bank');
+        }
+        else if((xPos < storeXPos - 30 || yPos < storeYPos - 30 || xPos > storeXPos + 40 || yPos > storeYPos + 30) && storeEvent == 2){
+            text("Gotta buy some groceries", 850, 700);           
+            fill(r, g, b);
+            ellipse(storeXPos, storeXPos, 10, 30);
+            console.log('player is at the store');
+        }
+        else if(xPos < pizzaXPos - 30 || yPos < pizzaYPos - 30 || xPos > pizzaXPos + 40 || yPos > pizzaYPos + 30){
+            text("It's a special offer at the pizzaria", 850, 700);
+            fill(r, g, b);
+            ellipse(pizzaXPos, pizzaYPos);
+            console.log('player is at the pizza shop');
+        }
+    
+    
+      if (state == 2) {
+        let div = createDiv('').size(500, 500);
+        div.style('background-color', 'white');
+        div.center();
+       
+        
+        }
+
+ }
 }
